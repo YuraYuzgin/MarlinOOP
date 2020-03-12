0973de9f17a4f4a72fdb4611cd326290a4654a77
Yuriy <yura.yuzgin@yandex.ru>
2020-03-12 15:33:18 +0300

    Закончен компонент Database

--- Database.php
+++ Database.php
@@ -2,14 +2,142 @@
 
 class Database
 {
-
-    public $pdo;
+    private static $instance = null;
+    private $pdo, $query, $error = false, $results, $count;
 
     public function __construct()
     {
-        $this->pdo = new PDO('mysql:host=localhost;dbname=marlin_oop_db', 'root', '');
+        try{
+            $this->pdo = new PDO('mysql:host=localhost;dbname=marlin_oop_db', 'root', '');
+        } catch (PDOException $exception) {
+            die($exception->getMessage());
+        }
+        
     }
 
+    public static function getInstance()
+    {
+        if(!isset(self::$instance)) {
+            self::$instance = new Database;
+        }
+        return self::$instance;
+    }
+
+    public function query($sql, $params = [])
+    {
+        $this->error = false;
+        $this->query = $this->pdo->prepare($sql);
+
+        if(count($params)) {
+            $i = 1;
+            foreach ($params as $param) {
+                $this->query->bindValue($i, $param);
+                $i++;
+            }
+        }
+
+        if(!$this->query->execute()) {
+            $this->error = true;
+        } else {
+            $this->results = $this->query->fetchAll(PDO::FETCH_OBJ);
+            $this->count = $this->query->rowCount();
+        }
+        
+        
+        return $this;
+    }
+
+    public function error()
+    {
+        return $this->error;
+    }
+
+    public function results()
+    {
+        return $this->results;
+    }
+
+    public function count()
+    {
+        return $this->count;
+    }
+
+    public function get($table, $where = [])
+    {
+        return $this->action('SELECT *', $table, $where);
+    }
+
+    public function delete($table, $where = [])
+    {
+        return $this->action('DELETE', $table, $where);
+    }
+
+    public function action($action, $table, $where = [])
+    {
+        if(count($where) === 3) {
+
+            $operators = ['=', '>', '<', '>=', '<='];
+
+            $field = $where[0];
+            $operator = $where[1];
+            $value = $where[2];
+
+            if(in_array($operator, $operators)) {
+
+                $sql = "{$action} FROM {$table} WHERE {$field} {$operator} ?";
+                if(!$this->query($sql, [$value])->error()) {
+                    return $this;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public function insert($table, $fields = [])
+    {
+        $values = '';
+        foreach ($fields as $field) {
+            $values .= "?,";
+        }
+        $values = rtrim($values, ',');
+
+        $sql = "INSERT INTO {$table} (`" . implode('`, `',array_keys($fields)) . "`) VALUES (" . $values . ")";
+
+        if(!$this->query($sql, $fields)->error()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function update($table, $id, $fields = [])
+    {
+        $set = '';
+        foreach ($fields as $key => $field) {
+            $set .= "{$key} = ?,";
+        }
+
+        $set = rtrim($set, ',');
+
+        $sql = "UPDATE {$table} SET {$set} WHERE id = {$id}";
+
+        if(!$this->query($sql, $fields)->error()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function first()
+    {
+        return $this->results()[0];
+    }
+
+
+
+
+    /*
     public function getAll($table)
     {
         $sql = "SELECT * FROM $table";
@@ -65,4 +193,5 @@ class Database
         $statement = $this->pdo->prepare($sql);
         $statement->execute([':id' => $id]);
     }
+    */
 }
--- test.php
+++ test.php
@@ -1,14 +1,36 @@
 <?php
 
-require 'Database.php';
+require_once 'Database.php';
 
-$query = new Database;
+//$users = Database::getInstance()->query("SELECT * FROM users WHERE username IN (?, ?)", ['John Doe', 'Jane Koe']);
+//$users = Database::getInstance()->get('users', ['username', '=','John Doe']);
+//$users = Database::getInstance()->delete('users', ['username', '=','Jane Koe']);
 
-$result = $query->getAll('test');
+/*Database::getInstance()->update('users', 5, [
+    'username' => 'Aldous Huxley',
+    'password' => 'newpassword'
+]);*/
+
+$users = Database::getInstance()->get('users', ['username', '=','John Doe']);
+echo $users->first()->username;
+
+
+/*if($users->error()) {
+    echo 'we have an error';
+} else {
+    foreach ($users->results() as $user) {
+        echo $user->username . '<br>';
+    }
+}*/
+
+
+//$query = new Database;
+
+//$result = $query->getAll('test');
 //$result = $query->getOne('test', 2);
 //$query->insert('test', ['name' => 'Olga', 'comments' => 'text']);
 //$query->update('test', ['comments' => 'new text'], 1);
 //$query->delete('test', 5);
-var_dump($result);
+//var_dump($result);
 
 ?>
