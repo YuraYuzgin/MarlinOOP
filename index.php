c6692c0a18f9c1ab5ef38d5980ce624405415974
Yuriy <yura.yuzgin@yandex.ru>
2020-03-17 01:31:55 +0300

    Добавлены компоненты Token и Session.

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 
+session_start();
+
 require_once 'Database.php';
 require_once 'Config.php';
 require_once 'Validate.php';
 require_once 'Input.php';
+require_once 'Token.php';
+require_once 'Session.php';
 
 $GLOBALS['config'] = [
     'mysql' => [
@@ -11,34 +15,40 @@ $GLOBALS['config'] = [
         'username' => 'root',
         'password' => '',
         'database' => 'marlin_oop_db'
+    ],
+
+    'session' => [
+        'token_name' => 'token'
     ]
 ];
 
 if(Input::exists()) {
-    $validate = new Validate();
+    if(Token::check(Input::get('token'))) {
+        $validate = new Validate();
 
-    $validation = $validate->check($_POST, [
-        'username' => [
-            'required' => true,
-            'min' => 2,
-            'max' => 15,
-            'unique' => 'users'
-        ],
-        'password' => [
-            'required' => true,
-            'min' => 3
-        ],
-        'password_again' => [
-            'required' => true,
-            'matches' => 'password'
-        ]
-    ]);
+        $validation = $validate->check($_POST, [
+            'username' => [
+                'required' => true,
+                'min' => 2,
+                'max' => 15,
+                'unique' => 'users'
+            ],
+            'password' => [
+                'required' => true,
+                'min' => 3
+            ],
+            'password_again' => [
+                'required' => true,
+                'matches' => 'password'
+            ]
+        ]);
 
-    if($validation->passed()) {
-        echo 'passed';
-    } else {
-        foreach ($validation->errors() as $error) {
-            echo $error . "<br>";
+        if($validation->passed()) {
+            echo 'passed';
+        } else {
+            foreach ($validation->errors() as $error) {
+                echo $error . "<br>";
+            }
         }
     }
 }
@@ -61,6 +71,8 @@ if(Input::exists()) {
         <input type="text" name="password_again">
     </div>
 
+    <input type="hidden" name="token" value="<?php echo Token::generate();?>">
+    
     <div class="field">
         <button type="submit">Submit</button>
     </div>
