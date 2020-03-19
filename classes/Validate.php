1c19a2d907e0095f162916ff9b58f26a369eae5e
Yuriy <yura.yuzgin@yandex.ru>
2020-03-19 18:22:08 +0300

    Испралена структура проекта, добавлены компонеты User и Redirect, добавлено в БД поле email, новое правило валидации.

--- classes/Validate.php
+++ classes/Validate.php
@@ -43,6 +43,12 @@ class Validate
                                 $this->addError("{$item} already exists.");
                             }
                         break;
+
+                        case 'email':
+                            if(!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+                                $this->addError("{$item} is not an email");
+                            }
+                        break;
                     }
                 }
             }
--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'Session.php';
+require_once 'classes/Session.php';
 session_start();
 
 echo Session::flash('success');
